e45f4fd121eda71a4a1bb39459e64d134548b5e4
Meera <meerashaik1708@gmail.com>
2025-09-07 22:57:29 +0530

    The ascii values of characters.

---
+++ ascii_value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    char ch;
+    printf("Enter a charcter:\n");
+    scanf("%c",&ch);
+    printf("The ascii value %c is:%d\n",ch,ch);
+    return 0;
+}
