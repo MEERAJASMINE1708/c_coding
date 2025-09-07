8d885400fc125ec6aad58f4b479399ee6a300b75
Meera <meerashaik1708@gmail.com>
2025-09-07 22:36:40 +0530

    operations with float values

--- operations.c
+++ operations.c
@@ -1,20 +1,44 @@
-//wap apply oerations on integer values
+//wap apply operations on integer values
+
+// #include<stdio.h>
+// #include<conio.h>
+// int main(){
+//     int a,b,c;
+//     printf("Enter two numbers:\n");
+//     scanf("%d%d",&a,&b);
+//     c=a+b;
+//     printf("\n The Addition is:%d",c);
+//     c=a-b;
+//     printf("\n The Subtraction is:%d",c);
+//     c=a*b;
+//     printf("\n The Multliplication is:%d",c);
+//     c=a/b;
+//     printf("\n The Division is:%d",c);
+//     c=a%b;
+//     printf("\n The Modulo DIvision is:%d",c);
+   // return 0;
+//}
+
+
+
+
+//wap apply operations on float values
 
 #include<stdio.h>
 #include<conio.h>
+
 int main(){
-    int a,b,c;
-    printf("Enter two numbers:\n");
-    scanf("%d%d",&a,&b);
+    float a,b,c;
+    printf("Enter two float numbers:");
+    scanf("%f%f",&a,&b);
     c=a+b;
-    printf("\n The Addition is:%d",c);
+    printf("\n The addition is:%f",c);
     c=a-b;
-    printf("\n The Subtraction is:%d",c);
+    printf("\n The Subtraction is:%f",c);
     c=a*b;
-    printf("\n The Multliplication is:%d",c);
+    printf("\n The Multliplication is:%f",c);
     c=a/b;
-    printf("\n The Division is:%d",c);
-    c=a%b;
-    printf("\n The Modulo DIvision is:%d",c);
+    printf("\n The Division is:%f",c);
+    // Modulo division not accepted in case of float values.
     return 0;
 }
