69f32c9066d3c55a02d2d6543d679b64e43b0e73
Meera <meerashaik1708@gmail.com>
2025-09-08 18:43:58 +0530

    Swapping of two numbers

--- swapping.c
+++ swapping.c
@@ -1,17 +1,40 @@
-//wap to swap two numbers using tem variable.
+//wap to swap two numbers using temp variable.
+
+// #include<stdio.h>
+// #include<conio.h>
+
+// int main(){
+//     int a,b,temp;
+//     printf("Enter two values a and b:\n");
+//     scanf("%d%d",&a,&b);
+//     printf("\n Numbers before swapping:%d %d",a,b);
+//     temp=a;
+//     a=b;
+//     b=temp;
+//     printf("\n Numbers after swapping:%d %d",a,b);
+//     return 0;
+
+// }
+
+
+
+//wap to swap two numbers without temp variable.
+
 
 #include<stdio.h>
 #include<conio.h>
 
 int main(){
-    int a,b,temp;
-    printf("Enter two values a and b:\n");
-    scanf("%d%d",&a,&b);
-    printf("\n Numbers before swapping:%d %d",a,b);
-    temp=a;
-    a=b;
-    b=temp;
-    printf("\n Numbers after swapping:%d %d",a,b);
-    return 0;
+    int a,b;
+    printf("Enter two int type values:\n");
+    scanf("%d%d", &a, &b);
+    printf("The values of a and b before swapping:%d %d\n",a,b);
+    a=a + b;     // add both numbers and store it in a.
+    b=a - b;     //original b from sum to get original a.
+    a=a - b;     //subtract new b from sum to get original b.
 
+    printf("The values of a and b after swapping:%d %d\n",a,b);
+    return 0;
 }
+
+
