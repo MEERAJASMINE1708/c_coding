ca5d2513cec9dc4f6dde02036bb1dd5f00fe3c92
Meera <meerashaik1708@gmail.com>
2025-09-09 18:52:55 +0530

    vowel or consonant.

--- vowel_consonant.c
+++ vowel_consonant.c
@@ -1,19 +1,43 @@
 //wap given character is an vowel or consonant.
 
+// #include<stdio.h>
+// #include<conio.h>
+
+
+// int main(){
+//     char ch;
+//     printf("Enter a character:\n");
+//     scanf("%c",&ch);
+//     if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+//         printf("%c is an vowel.",ch);
+//     }
+//     else{
+//         printf("%c is an consonant.",ch);
+//     }
+//     return 0;
+
+// }
+
+
+
+
 #include<stdio.h>
 #include<conio.h>
 
 
 int main(){
     char ch;
+    int lowercase,uppercase;
     printf("Enter a character:\n");
     scanf("%c",&ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+    lowercase=(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
+    uppercase=(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U');
+    
+    if(lowercase||uppercase){
         printf("%c is an vowel.",ch);
     }
     else{
         printf("%c is an consonant.",ch);
     }
     return 0;
-
 }
