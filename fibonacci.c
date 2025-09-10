a86daffbc7f260f7aeadef16fbd502851099284c
Meera <meerashaik1708@gmail.com>
2025-09-10 18:09:07 +0530

    fibonacci series.

---
+++ fibonacci.c
@@ -0,0 +1,21 @@
+//wap to generate fibonacci series.
+
+
+#include<stdio.h>
+#include<conio.h>
+
+
+int main(){
+    int i,n,t1=0,t2=1,next_term;
+    printf("Enter an integer:\n");
+    scanf("%d",&n);
+    printf("The FIbonacci Series is:\n");
+    for(i=1;i<=n;i++){
+        
+        printf("%d\n",t1);
+        next_term=t1+t2;
+        t1=t2;
+        t2=next_term;
+    }
+    return 0;
+}
