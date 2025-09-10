a6c91aaa6c15661925fdbcb4936b50e382ee4dce
Meera <meerashaik1708@gmail.com>
2025-09-10 11:30:30 +0530

    areas of circle,rectangle,triangle.

---
+++ areas.c
@@ -0,0 +1,50 @@
+//wap to find area of circle.
+
+
+// #include<stdio.h>
+// #include<conio.h>
+// #define pi 3.14
+
+// int main(){
+//     int r;
+//     float area;
+//     //const float pi = 3.14;
+//     printf("Enter radius of circle:\n");
+//     scanf("%d",&r);
+//     area=pi*r*r;
+//     printf("The area of circle is:%.2f",area);
+//     return 0;
+// }
+
+
+
+//wap to display the area of rectangle.
+
+// #include<stdio.h>
+// #include<conio.h>
+
+
+// int main(){
+    
+//     float l,b,area;
+//     printf("Enter length and beadth:\n");
+//     scanf("%f%f",&l,&b);
+//     area=l*b;
+//     printf("The area of rectangle is:%f",area);
+//     return 0;
+// }
+
+
+
+//wap to find the area of triangle.
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float b,h,area;
+    printf("Enter base and height of triangle:\n");
+    scanf("%f%f",&b,&h);
+    area=0.5*b*h;
+    printf("The area of triangle is:%.2f",area);
+    return 0;
+}
