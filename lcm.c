3554c6eb5736a24774b35e9d4c31e4d1ce7d2445
Meera <meerashaik1708@gmail.com>
2025-09-21 23:58:36 +0530

    lcm of two numbers.

---
+++ lcm.c
@@ -0,0 +1,21 @@
+//wap to write lcm of two numbers.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int n1,n2,max;
+    printf("Enter two numbers:\n");
+    scanf("%d%d",&n1,&n2);
+    
+    max=(n1>n2)?n1:n2;
+     
+    while(1){ 
+    if(max%n1==0 && max%n2==0){
+        printf("The lcm of %d and %d is:%d",n1,n2,max);
+        break;
+    }
+    max++;
+    }
+    return 0;
+}
